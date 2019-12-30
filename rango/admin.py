3c6a19177249cc5a97cb8881da6e63c21c776a3a
格桑宇珍 <gsyz@gesangyuzhendeMacBook-Pro.local>
2019-12-31 01:53:31 +0800

    first commit

---
+++ rango/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin 
+from rango.models import Category, Page
+class CategoryAdmin(admin.ModelAdmin): 
+    prepopulated_fields = {'slug':('name',)}
+admin.site.register(Category) 
+admin.site.register(Page)
---
+++ rango/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from rango.models import Category
+from rango.models import Page
+from rango.forms import CategoryFrom
+
+def add_category(request):
+    form = CategoryFrom()
+    if request.method == 'POST' :
+        form = CategoryFrom(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print(form.errors)
+    return render(request, 'rango/add_category.html', {'form': form})
+
+def index(request): 
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {} 
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!' 
+    context_dict['categories'] = category_list
+    return render(request, 'rango/index.html', context_dict)
+
+def show_category(request, category_name_slug): 
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category 
+    except Category.DoesNotExist:
+        context_dict['category'] = None 
+        context_dict['pages'] = None
+    return render(request, 'rango/category.html', context_dict)
+
+def about(request):
+    return HttpResponse("Hello World!<a href='/rango/'>Index</a>")
